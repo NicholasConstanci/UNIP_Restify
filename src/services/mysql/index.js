04078fb7271b6f4acf59eaa8696340c6679ca697
NikoAPS <nicholasconstanci@gmail.com>
2017-10-29 21:43:10 -0200

    And now it is online! I hope! Upl3

--- src/services/mysql/index.js
+++ src/services/mysql/index.js
@@ -2,10 +2,12 @@
 const mysqlServer = require('mysql')
 
 const connection = mysqlServer.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'restful_ws'
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'b5c4a735bdcf23',
+  password: '10a7d23c',
+  database: 'heroku_eb0161150ae89ff'
+  // mysql://b5c4a735bdcf23:10a7d23c@us-cdbr-iron-east-05.cleardb.net/heroku_eb0161150ae89ff?reconnect=true
+  // pw: 10a7d23c
 })
 
 const errorHandler = (error, msg, rejectFunction) => {
